682824a07bed8a5986cf45af452c653e468e9e8c
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-10-28 21:17:31 -0300

    arreglo detalles de tarjeta menú

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -10,33 +10,29 @@ class Menu extends Component {
     }
 
     handleClick = (e) => {
-        return this.setState(e.target.id)
+        this.setState({color: e.target.id})
     }
 
     render () {
-        const { color } = this.state.color;
+        // const { color } = this.state;
         return (
             <Fragment>
-                <Card body className="menuCard">
-                    <CardTitle>Saludos, estudiante</CardTitle>
+                <Card body className="float-right menuCard">
+                    <CardTitle><h2>¡Bienvenide!</h2></CardTitle>
                     <CardText>Marca tus tiendas favoritas con un color de tu preferencia.</CardText>
                         <ButtonToolbar aria-label="Toolbar with button groups">
                             <ButtonGroup className="mr-2" aria-label="First group">
                                 <Button 
                                 id="red" 
-                                userIcon={color} 
                                 onClick={this.handleClick} 
                                 color="danger">{storeIcon}</Button>
                                 <Button id="green" 
-                                userIcon={color} 
                                 onClick={this.handleClick} 
                                 color="success">{storeIcon}</Button>
                                 <Button id="yellow" 
-                                userIcon={color} 
                                 onClick={this.handleClick} 
                                 color="warning">{storeIcon}</Button>
                                 <Button id="grey" 
-                                userIcon={color} 
                                 onClick={this.handleClick} 
                                 color="secondary">{storeIcon}</Button>
                             </ButtonGroup>
