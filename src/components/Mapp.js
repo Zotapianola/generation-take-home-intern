7d90c3648d8cf82cde55cd40e8ffca1919afb67f
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-11-11 11:53:40 -0300

    se agrega router a mapp.js

--- src/components/Mapp.js
+++ src/components/Mapp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Map, TileLayer } from 'react-leaflet';
 import Header from './Layout/Header';
 import StoreMarkers from './Store/StoreMarkers';
@@ -28,19 +29,23 @@ class Mapp extends Component {
     const position = [location.lat, location.lng];
 
     return (
-      <div className="mapGrandParent">
-        <Header />
-        <div className="mapParent">
-          <Map className="map" center={position} zoom={zoom}>
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Menu getColor={this.getUserColor} />
-            <StoreMarkers iconColor={iconColor} />
-          </Map>
-        </div>
-      </div>
+      <Router>
+        <Route exact path="/">
+          <div className="mapGrandParent">
+            <Header />
+            <div className="mapParent">
+              <Map className="map" center={position} zoom={zoom}>
+                <TileLayer
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Menu getColor={this.getUserColor} />
+                <StoreMarkers iconColor={iconColor} />
+              </Map>
+            </div>
+          </div>
+        </Route>
+      </Router>
     );
   }
 }
