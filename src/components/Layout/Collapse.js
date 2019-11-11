a11f241ae3ffe0c2b1b4167363d470851f298130
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-11-11 10:53:23 -0300

    tabla incorporada a tarjeta colapsable

--- src/components/Layout/Collapse.js
+++ src/components/Layout/Collapse.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import { UncontrolledCollapse, Button, Card } from 'reactstrap';
 
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>My Store 1</td>
+        <td>X</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>My Store 2</td>
+        <td>X</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>My Store 3</td>
+        <td>X</td>
+      </tr>
+    </tbody>
+  );
+};
+
 const Collapsable = () => {
   return (
     <React.Fragment>
-      <Button
-        color="default"
-        id="toggler"
-        style={{ padding: '0px', width: '178px', height: '30px' }}
-      >
-        <i
-          className="fas fa-sort-down"
-          style={{
-            width: '178px',
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        ></i>
+      <Button color="default" id="toggler">
+        <i className="fas fa-sort-down" id="collapse-icon"></i>
       </Button>
       <UncontrolledCollapse toggler="#toggler">
         <Card className="userStoreCard">
-          <ul className="userStoreList list-group">
-            <li className="list-group-item-success">
-              Red Barn Stores 3858-CUAJIMALPA
-            </li>
-            <li className="list-group-item-secondary">
-              Red Barn Stores 1770-PATIO SANTA FE
-            </li>
-            <li className="list-group-item-warning">KAYCHEE 154-Xochimilco</li>
-          </ul>
+          <table>
+            <TableHeader />
+            <TableBody />
+          </table>
         </Card>
       </UncontrolledCollapse>
     </React.Fragment>
